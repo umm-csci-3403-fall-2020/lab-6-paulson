c763814216f362778f318e615797b241b5bd134c
halleypaulson <70215918+halleypaulson@users.noreply.github.com>
2020-10-19 19:18:23 -0500

    Added separate Read and Write threads and uploaded for testing

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,23 +1,75 @@
 package echoserver;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.Socket;
+import java.net.*;
+import java.io.*;
 
 public class EchoClient {
-	public static final int PORT_NUMBER = 6013;
 
-	public static void main(String[] args) throws IOException {
-		EchoClient client = new EchoClient();
-		client.start();
-	}
+    public static final int portNumber = 6013;
 
-	private void start() throws IOException {
-		Socket socket = new Socket("localhost", PORT_NUMBER);
-		InputStream socketInputStream = socket.getInputStream();
-		OutputStream socketOutputStream = socket.getOutputStream();
+    public static void main(String[] args) throws IOException {
+        String server;
+        if (args.length == 0) {
+            server = "127.0.0.1";
+        } else {
+            server = args[0];
+        }
 
-		// Put your code here.
-	}
+        try {
+            Socket socket = new Socket(server, portNumber);
+
+            Thread outputWriter = new Writer(socket);
+            Thread inputReader = new Reader(socket);
+
+        } catch (ConnectException ce) {
+            System.out.println("We were unable to connect to " + server);
+            System.out.println("You should make sure the server is running.");
+        } catch (IOException ioe) {
+            System.out.println("We caught an unexpected exception");
+            System.err.println(ioe);
+        }
+    }
+
+    public static class Reader extends Thread implements Runnable{
+        //Only for reading from stdin and writing to socket
+        private final Socket socket;
+
+        public Reader(Socket s){
+            this.socket = s;
+        }
+
+        public void run() {
+            try{
+                OutputStream socketOut = socket.getOutputStream();
+
+                int data;
+                while((data = System.in.read()) != -1){
+                    socketOut.write(data);
+                }
+                socket.shutdownOutput();
+            }
+            catch (IOException e){ e.printStackTrace(); }
+        }
+    }
+
+    public static class Writer extends Thread implements Runnable{
+        //Only for reading from socket and writing to stdout
+        private final Socket socket;
+
+        public Writer(Socket s) {
+            this.socket = s;
+        }
+
+        public void run(){
+            try{
+            InputStream socketIn = socket.getInputStream();
+
+                int data;
+                while((data = socketIn.read()) != -1) {
+                System.out.write(data);
+                }
+                System.out.flush();
+            }
+            catch (IOException e) { e.printStackTrace(); }
+        }
+    }
 }
